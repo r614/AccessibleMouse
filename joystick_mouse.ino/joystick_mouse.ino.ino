d637ddd6e19b330f3f91821a9f922890873b7391
Kevin Wang <k.w.gunner62@gmail.com>
2018-12-01 14:44:16 -0800

    added clicking functionality

--- joystick_mouse.ino/joystick_mouse.ino.ino
+++ joystick_mouse.ino/joystick_mouse.ino.ino
@@ -1,35 +1,39 @@
+
 /*
   JoystickMouseControl
-
   Controls the mouse from a joystick on an Arduino Leonardo, Micro or Due.
   Uses a pushbutton to turn on and off mouse control, and a second pushbutton
   to click the left mouse button.
-
   Hardware:
   - 2-axis joystick connected to pins A0 and A1
   - pushbuttons connected to pin D2 and D3
-
   The mouse movement is always relative. This sketch reads two analog inputs
   that range from 0 to 1023 (or less on either end) and translates them into
   ranges of -6 to 6.
   The sketch assumes that the joystick resting values are around the middle of
   the range, but that they vary within a threshold.
-
   WARNING: When you use the Mouse.move() command, the Arduino takes over your
   mouse! Make sure you have control before you use the command. This sketch
   includes a pushbutton to toggle the mouse control state, so you can turn on
   and off mouse control.
-
   created 15 Sep 2011
   updated 28 Mar 2012
   by Tom Igoe
-
   This example code is in the public domain.
-
   http://www.arduino.cc/en/Tutorial/JoystickMouseControl
 */
+#include <Mouse.h>
+#include <mpx5700.h>
 
-#include "Mouse.h"
+//Call the first instance of the class so you have the ability to call built in functions of library
+mpx5700 mpx;
+
+
+// Here we would have the data pin 1 connected to A0, we then set that to the analog zero on the arduino board.
+
+int sensorPin = A0;    // Select input pin for the pressure sensor
+int sensorValue = 0;   // Variable stores value coming from the sensor
+boolean didClick = false;
 
 // set pin numbers for switch, joystick axes, and LED:
 const int switchPin = 2;      // switch to turn on and off mouse control
@@ -37,6 +41,7 @@ const int mouseButton = 3;    // input pin for the mouse pushButton
 const int xAxis = A1;         // joystick X axis
 const int yAxis = A2;         // joystick Y axis
 const int ledPin = 5;         // Mouse control LED
+
 
 // parameters for reading the joystick:
 int range = 12;               // output range of X or Y movement
@@ -55,6 +60,32 @@ void setup() {
 }
 
 void loop() {
+
+  int voltageReading = analogRead(sensorPin);
+  if (voltageReading > 40 || voltageReading < 20) {
+   
+  } else {
+  
+    if (voltageReading < 34 && voltageReading > 29){
+      if(Mouse.isPressed(MOUSE_LEFT)) {
+        Mouse.release(MOUSE_LEFT);
+      }
+      if(Mouse.isPressed(MOUSE_RIGHT)) {
+        Mouse.release(MOUSE_RIGHT);
+      }
+      didClick = false; 
+  }
+    if (voltageReading > 34 && didClick == false) {
+      Mouse.press(MOUSE_LEFT);
+      didClick = true;
+    }
+    if (voltageReading < 29 && didClick == false) {
+      Mouse.press(MOUSE_RIGHT);
+      didClick = true;
+    }
+  }
+  
+   
   // read the switch:
   int switchState = HIGH;
   // if it's changed and it's high, toggle the mouse state:
